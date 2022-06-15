ba1fb875d24bc2a884fa00e5c4a6610ae9b0d315
lucyalvarado4692 <104829949+lucyalvarado4692@users.noreply.github.com>
2022-06-15 15:46:13 +0200

    Add script insert
    Esta solo la función insertar el nombre, falta ahora reeditar para agregar las funciones creadas por Daniel.

--- script.js
+++ script.js
@@ -1,36 +1,64 @@
-const players=["Aide", "Dani", "Deimos"];
-const buttonStart= document.getElementById("#controls");
-buttonStart.onclick= startGame;
-document.getElementById("#controls").onclick= startGame;
-const screen= document.getElementById("screengame");
-// console.log(players);
-// const test = checkName("Dani"):
-// console.log(test);
-function checkName(name){
-    let nameInArray= players.includes(name);
-    return nameInArray;
+//function para insertar nombre 
+//Pasos
+//1 clik (desencadenar una accion con buton on click)
+//2 get value del input (coger lo que ingresron en mi input)
+//3 covertir esa cadena en minuscula ()
+//4 repasar los onjetos (foreach)del array de mi output
+//5 verificar si la cadena anterior esta incluida
+//si el include is true que arroje un alert con el mensaje "El nombre de este jugador ya esta registrado"
+//si el include is false que ejecute la funcion de agregar ese valor en el array de my output
+const buttonInsert = document.getElementById("insert")
+
+buttonInsert.onclick = insertName;
+
+let Output= ["patricia", "lucy", "daniel", "cesar", "toufick"]
+//let description=document.getElementById ("player")
+//description.innerHTML= Output;
+
+
+function insertName(){
+    console.log("paso 1 listo")
+    let insertedName=document.getElementById("write").value;
+    console.log(insertedName + " " + "paso 2 listo")
+    insertedName = insertedName.toLowerCase();
+    console.log(insertedName + " " + "paso 3 listo")
+    while (Output.includes(insertedName)){
+        alert("Error, ya existe este jugador")
+        Output++
+    }
+    while (!Output.includes(insertedName)){
+        alert("Bienvenido")
+        let OutputMod=insert()
+            function insert(){
+                Output.push(insertedName)
+                return (Output)
+            }
+        document.getElementById("player").innerHTML=OutputMod;
+        Output++
+    }
+    /*let OutputVerified=verified()
+    function verified(){
+        Output.includes(insertedName)
+        console.log("true") 
+    }
+    /*Output.forEach(function(word) {
+        console.log("paso 4 listo");
+        if (word.includes(insertedName)){
+            console.log("paso 5 listo")
+            alert("Error, ya existe este jugador")
+            console.log("paso 6 listo") 
+        }
+        else{
+            let OutputMod=insert()
+            function insert(){
+                Output.push(insertedName)
+                return (Output)
+            }
+        document.getElementById("player").innerHTML=OutputMod;
+        }
+    });*/
+    
+    
 }
-//const test2=printMessage("bieeeeeen");
-function printMessage(text){
-    window.alert(text);
-}
-//const test3= deleteNameOfArray("Dani");
-//console.log(players)
-function deleteNameOfArray(name){
-    let  deletedPlayer= players.indexOf(name);
-    players.splice( deletedPlayer, 1 );
-}
-//const test4=printNameInScreen("Dani")
-function printNameInScreen(randonName){
-    document.getElementById("screengame").innerHTML=`<h1 style="color: white">${randonName}</h1>`
-}
-function startGame(){
-    const ramdomName = players[Math.floor(Math.random() * players.length)];
-    printNameInScreen(ramdomName);
-    deleteNameOfArray(ramdomName);
-}
-const test4=showSacrificedPlayer("Dani")
-function showSacrificedPlayer(player_name) {
-    let screen_game = document.getElementById('screen-game')
-    screen_game.innerHTML = `<div class="SacrificedPlayer"><p>El jugador ${player_name} ha sido eliminado</p></div>`;
-}
+//document.getElementById ("player").innerHTML=Output.push(insertedName);
+            //return Output;
