e46c44451a5ab1917b19bc98fec97c1c2fb71e03
Patricia Rodríguez <104829537+prlongoria@users.noreply.github.com>
2022-06-17 10:45:41 +0200

    Definitivo

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const players=["Kitty","MOTICA","misi","Chandler","Joey", "Goofy","Kira","guti","vilma", "dorito","Deimos"];
-const buttonInsert = document.getElementById("insert")//boton insert
+const buttonInsert = document.getElementById("insert");
 buttonInsert.onclick = insertName;
 const buttonStart= document.getElementById("controls");
 buttonStart.onclick= startGame;
@@ -9,35 +9,43 @@ const screen= document.getElementById("screen-game");
 const buttonModify= document.getElementById("changeName");
 buttonModify.onclick=changeName;
 const playersStart = printArrayInPanel(players);
+
 function checkName(name){
     let nameInArray= players.includes(name);
     return nameInArray;
 }
+
 function printMessage(text){
     window.alert(text);
 }
+
 function deleteNameOfArray(name){
-    let  deletedPlayer= players.indexOf(name);
+    let deletedPlayer= players.indexOf(name);
     players.splice( deletedPlayer, 1 );
     printArrayInPanel(players);
 }
+
 function startGame(){
     const ramdomName = players[Math.floor(Math.random() * players.length)];
-    showSacrificedPlayer(ramdomName);
+    setTimeout(showSacrificedPlayer, 2000, ramdomName);    
     deleteNameOfArray(ramdomName);
     if (players.length===1){
-        winGame(players);
+        setTimeout(winGame, 2000, players);        
     }
 }
+
 function showSacrificedPlayer(player_name) {
     screen.innerHTML = `<div class="SacrificedPlayer"><h1>${player_name} has sido abducido</h1></div>`;
 }
+
 function winGame(winName){
     screen.innerHTML = `<div class="winner" style="background-image: url(/imagenes/fiesta.gif)"><h1 >${winName} eres el único superviviente</h1><button id="reboot" onclick="toReset()" style = "margin-left: 28vw;">reiniciar</button></div>`;     
 }
+
 function toReset(){
    return location.reload();
 }
+
 function changeName(){
     const name=document.getElementById("writeName").value;
      const checkedName=checkName(name);
@@ -48,6 +56,7 @@ function changeName(){
         printMessage(text);
     }
 }
+
 function checkNewName(name){    
     const newName=window.prompt("Introduce el nuevo nombre");
     const checkedNewName=checkName(newName);    
@@ -61,6 +70,7 @@ function checkNewName(name){
        return players
         }
 }
+
 function printArrayInPanel () {
     const participants = document.getElementById("participants");
     participants.innerHTML="";
@@ -68,6 +78,7 @@ function printArrayInPanel () {
         participants.innerHTML+= `<p>${element}</p>`; 
     })
 }
+
 function deleteName (){ 
     let writeName = document.getElementById("writeName").value;    
     let checkedName = checkName(writeName);
@@ -78,6 +89,7 @@ function deleteName (){
         printMessage ("El nombre no está en la lista");
     }
 }
+
 function insertName(){
      let insertedName = document.getElementById("writeName").value;
      let nameVerified = checkName(insertedName);    
@@ -91,3 +103,5 @@ function insertName(){
         }
     }
 }
+
+
