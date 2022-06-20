0fba477cce7363b063900ef67c8c293e15f7ce9f
prlongoria <prlongoria@gmail.com>
2022-06-20 17:02:52 +0200

    proyecto finalizado completo

--- script.js
+++ script.js
@@ -1,52 +1,107 @@
-const players=["Aide", "Dani", "Deimos"];
+const players=["Kitty","MOTICA","misi","Chandler","Joey", "Goofy","Kira","guti","vilma", "dorito","Deimos"];
+const buttonInsert = document.getElementById("insert");
+buttonInsert.onclick = insertName;
 const buttonStart= document.getElementById("controls");
-const buttonDelete= document.getElementById("delete");
-const writeName= document.getElementById("writeName").value;
 buttonStart.onclick= startGame;
+let buttonDelete= document.getElementById("delete");
+buttonDelete.onclick = deleteName;
 const screen= document.getElementById("screen-game");
-// console.log(players);
-// const test = checkName("Dani"):
-// console.log(test);
+const buttonModify= document.getElementById("changeName");
+buttonModify.onclick=changeName;
+const playersStart = printArrayInPanel(players);
+
 function checkName(name){
     let nameInArray= players.includes(name);
     return nameInArray;
 }
-//const test2=printMessage("bieeeeeen");
+
 function printMessage(text){
     window.alert(text);
 }
-//const test3= deleteNameOfArray("Dani");
-//console.log(players)
+
 function deleteNameOfArray(name){
-    let  deletedPlayer= players.indexOf(name);
+    let deletedPlayer= players.indexOf(name);
     players.splice( deletedPlayer, 1 );
+    printArrayInPanel(players);
 }
+
 function startGame(){
     const ramdomName = players[Math.floor(Math.random() * players.length)];
-    showSacrificedPlayer(ramdomName);
+    setTimeout(showSacrificedPlayer, 2000, ramdomName);    
     deleteNameOfArray(ramdomName);
-    if(players.length===1){
-        winGame(players);
+    if (players.length===1){
+        setTimeout(winGame, 2000, players);        
     }
 }
-//const test4=showSacrificedPlayer("Dani")
+
 function showSacrificedPlayer(player_name) {
-    screen.innerHTML = `<div class="SacrificedPlayer"><p>El jugador ${player_name} ha sido eliminado</p></div>`;
+    screen.innerHTML = `<div class="SacrificedPlayer"><h1>${player_name} has sido abducido</h1></div>`;
 }
 
 function winGame(winName){
-    screen.innerHTML = `<div class="winner"><p>El jugador ${winName} ha sobrevivido</p><button id="reboot">reiniciar</button></div>`;     
+    screen.innerHTML = `<div class="winner" style="background-image: url(/imagenes/fiesta.gif)"><h1 >${winName} eres el único superviviente</h1><button id="reboot" onclick="toReset()" style = "margin-left: 28vw;">reiniciar</button></div>`;     
 }
+
 function toReset(){
-   document.getElementById("reboot").onclick= location. reload();
-   
-
+   return location.reload();
 }
 
-//const test7 = deleteName();
+function changeName(){
+    const name=document.getElementById("writeName").value;
+     const checkedName=checkName(name);
+     if (checkedName){
+        checkNewName(name);
+    }else{
+        const text="El nombre no existe en la lista"
+        printMessage(text);
+    }
+}
+
+function checkNewName(name){    
+    const newName=window.prompt("Introduce el nuevo nombre");
+    const checkedNewName=checkName(newName);    
+    if(checkedNewName){
+        const text="este nombre ya existe";
+        printMessage(text);
+    } else{
+        let indexOfNameToChange = players.indexOf(name);
+        players[indexOfNameToChange] = newName;
+        printArrayInPanel(players);
+       return players
+        }
+}
+
+function printArrayInPanel () {
+    const participants = document.getElementById("participants");
+    participants.innerHTML="";
+    players.forEach (element => {    
+        participants.innerHTML+= `<p>${element}</p>`; 
+    })
+}
 
 function deleteName (){ 
-
-    buttonDelete.onclick = checkName(writeName);
-    console.log(players);
+    let writeName = document.getElementById("writeName").value;    
+    let checkedName = checkName(writeName);
+    if (checkedName){
+        deleteNameOfArray(writeName);
+        printArrayInPanel();
+    } else {
+        printMessage ("El nombre no está en la lista");
     }
+}
+
+function insertName(){
+     let insertedName = document.getElementById("writeName").value;
+     let nameVerified = checkName(insertedName);    
+    if(insertedName !==""){     
+        if (nameVerified){
+        printMessage("El jugador ya está en la lista");
+        } else{
+            alert("Bienvenido");
+            players.push(insertedName);
+            printArrayInPanel();            
+        }
+    }
+}
+
+
